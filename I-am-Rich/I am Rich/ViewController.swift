9081a62af023af1515c17ee8add2027f07d30830
Ali Can Köse <ackose6@gmail.com>
2024-08-25 00:48:18 +0300

    initial commit

---
+++ I-am-Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Ali Can Köse on 29.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
